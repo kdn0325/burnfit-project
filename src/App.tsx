e756d927a0764345c4f6480d3c00ec02790bcb32
kdn0325 <dn10003@gmail.com>
2025-08-14 15:39:11 +0900

    feat: BottomTabNavigation 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BottomTabs from './navigation/BottomTabs';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <BottomTabs />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
