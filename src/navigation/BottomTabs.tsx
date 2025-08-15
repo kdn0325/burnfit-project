bc5d519f990473a3b6357a488f5049394747f6d5
kdn0325 <dn10003@gmail.com>
2025-08-15 18:35:26 +0900

    feat: 바텀탭 네비게이션에 vector icon 적용

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,4 +1,3 @@
-// src/navigation/BottomTabs.tsx
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
@@ -7,12 +6,34 @@ import {
   CalendarScreen,
   HomeScreen,
 } from '../screens';
+import Lucide from '@react-native-vector-icons/lucide';
+
+type RouteName = 'Home' | 'Calendar' | 'Library' | 'MyPage';
+type IconName = 'house' | 'calendar' | 'book-open' | 'user';
 
 const Tab = createBottomTabNavigator();
 
+const icons: Record<RouteName, IconName> = {
+  Home: 'house',
+  Calendar: 'calendar',
+  Library: 'book-open',
+  MyPage: 'user',
+};
+
+const getTabBarIcon = (routeName: RouteName, color: string, size: number) => {
+  return <Lucide name={icons[routeName]} size={size} color={color} />;
+};
 export default function BottomTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) =>
+          getTabBarIcon(route.name as RouteName, color, size),
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Library" component={LibraryScreen} />
